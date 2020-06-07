9a5b926da2a7845359650d89e54b252c14901600
windyan233 <2359034782@qq.com>
2020-06-07 17:28:03 +0800

    Site updated: 2020-06-07 17:28:02

--- js/click_show_text.js
+++ js/click_show_text.js
@@ -12,7 +12,7 @@ jQuery(document).ready(function ($) {
             "left": x,
             "position": "absolute",
             "font-weight": "bold",
-            "color": "#FF0000"
+            "color": "#F5FFFA"
         });
         $("body").append($i);
         $i.animate({
